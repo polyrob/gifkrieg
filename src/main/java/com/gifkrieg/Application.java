882fd96c494a26e28a0ca78a2ff58c3d0dc014fa
rob <robbiescheidt@gmail.com>
2017-04-09 20:48:18 -0700

    fixed config to allow for local/deployed startup

--- src/main/java/com/gifkrieg/Application.java
+++ src/main/java/com/gifkrieg/Application.java
@@ -3,13 +3,14 @@ package com.gifkrieg;
 /**
  * Created by Rob on 4/2/2017.
  */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application  extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
